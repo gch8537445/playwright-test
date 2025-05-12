d5756f597d7ac79f7285a652113a22e8d63cd507
great <chao.gao@imtpath.com>
2025-05-12 15:28:58 +0800

    Jenkinsfile

--- tests/greaty.spec.ts
+++ tests/greaty.spec.ts
@@ -2,15 +2,36 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://mobilev2.imtpath.net/?oauth=6&token=06ce919d-e0b9-453e-92e3-525ce964a015#/');
-  //await page.getByText('定位失败请手动输入').click();
-  //await page.getByRole('textbox', { name: '城市中文名或拼音' }).click();
-  //await page.getByRole('textbox', { name: '城市中文名或拼音' }).fill('大连');
-  //await page.locator('a').filter({ hasText: '大连市' }).click();
-  await page.getByText('大连腾讯大厦-1号门').nth(1).click();
-  await page.getByText('常用地址1大连腾讯大厦').click();
+
+  // 检测是否存在“定位失败请手动输入”并点击
+  const locationError = page.getByText('定位失败请手动输入');
+  // 等待元素可见
+  await locationError.waitFor({ state: 'visible' });
+  console.log('定位失败元素已出现');
+
+  // 等待1秒后点击
+  await page.waitForTimeout(1000);
+
+  if (await locationError.isVisible()) {
+    await locationError.click();
+    await page.getByRole('textbox', { name: '城市中文名或拼音' }).click();
+    await page.getByRole('textbox', { name: '城市中文名或拼音' }).fill('大连');
+    await page.locator('a').filter({ hasText: '大连市' }).click();
+    await page.getByRole('textbox', { name: '从哪出发' }).click();
+    await page.getByRole('textbox', { name: '从哪出发' }).fill('腾讯大厦');
+    await page.getByText('1号门').click();
+  }
+  else {
+    await page.getByText('大连腾讯大厦-1号门').nth(1).click();
+  }
+
+
   await page.locator('div').filter({ hasText: /^你要去哪儿\?$/ }).nth(1).click();
-  await page.getByText('七贤岭森林公园').click();
-  await page.getByText('同时呼叫 (预估1-5元)').click();
+  await page.getByRole('textbox', { name: '您要去哪' }).click();
+  await page.getByRole('textbox', { name: '您要去哪' }).fill('七贤岭');
+  await page.getByText('入口').click();
+  await page.getByText(/同时呼叫/).click();
   await page.getByRole('button', { name: '确认叫车' }).click();
-  await expect(page.locator('body')).toMatchAriaSnapshot(`- text: 用车时间不在限制范围内`);
+  await expect(page.getByText('下单限制已失效，不可再次下单，请重新登录！')).toBeVisible();
+  //await expect(page.locator('body')).toMatchAriaSnapshot(`- text: 用车时间不在限制范围内`);
 });
